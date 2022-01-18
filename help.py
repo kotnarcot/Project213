7b97a4e191634982b904d24bfc62c7e1689f66c8
FerterH <ferterhard0@gmail.com>
2022-01-18 16:00:50 +0500

    first commit

--- help.py
+++ help.py
@@ -5,7 +5,9 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/FerterH/django.git
 git push -u origin main
+
+
+ django-admin startproject taskmanager
+PS C:\Users\IT-12\PycharmProjects\Django> cd .\taskmanager\
+PS C:\Users\IT-12\PycharmProjects\Django\taskmanager> python manage.py runserver
 '''
-
-
-
