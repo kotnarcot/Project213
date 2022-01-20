48f53838cc67f9ef1db635b8ed69e2c1e887b86f
FerterH <ferterhard0@gmail.com>
2022-01-20 15:56:17 +0500

    20.01

--- help.py
+++ help.py
@@ -1,6 +1,6 @@
 '''
 git init
-git add README.md
+git add .
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/FerterH/django.git
@@ -10,4 +10,8 @@ git push -u origin main
  django-admin startproject taskmanager
 PS C:\Users\IT-12\PycharmProjects\Django> cd .\taskmanager\
 PS C:\Users\IT-12\PycharmProjects\Django\taskmanager> python manage.py runserver
+
+при создании нового приложения (в осовном) командой: python manage.py startapp Название
+За тем в папке этого проекта нужно создать файл urls.py
+
 '''
